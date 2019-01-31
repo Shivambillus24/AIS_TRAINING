dafbe2ce1e67931a44dbf7dd9430f9b957256831
Shivam Billus <billusshivam24@gmail.com>
2019-01-31 11:55:43 +0530

    ASSIGNMENT ON 31/01/2019

---
+++ firstexp.ps1
@@ -0,0 +1,21 @@
+﻿function divide(){                             #function to divide two numbers
+try {
+    $c = $num1/$num2
+    Write-Host ('The answer is :')       
+    $c                                       
+}
+catch [DivideByZeroException]{
+    Write-Host ('There is a divide by zero exception')  #divide by zero exception will occur
+}
+}
+try {
+Write-Host ('Enter the first numbers:')
+[INT]$num1 = Read-Host                      #input 1st number
+Write-Host ('Enter the second numbers:')
+[INT]$num2 = Read-Host                      #input 2nd number
+divide $num1, $num2                         #function call to divide two numbers
+}
+catch {
+    Write-Host ('Error has occured')      #general catch all if any other error occurres
+    Write-Host $_.Exception.Message
+    }
