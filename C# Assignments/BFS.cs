0cd6795f73172e506eddade051f658dfeb269f39
Shivambillus24 <47132484+Shivambillus24@users.noreply.github.com>
2019-02-04 21:03:31 +0530

    Add files via upload

---
+++ C# Assignments/BFS.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SB.BFS
+{
+    class Tree
+    {
+        public Node root;              //variaable node root to store root value
+        void BinaryTree()
+        {
+            root = null;
+        }
+        public void PrintBFS(Node root)   //function to print tree through BFS
+        {
+            int h = height(root);
+            int i;
+            for (i = 0; i < h; i++)        //for loop to print every level
+            {
+                PrintGivenLevel(root,i);  //function call to print specific 
+            }
+
+        }
+        public int height(Node root)      //function to calculate height of tree
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            else
+            {
+                int leftHeight = height(root.left);      //recurssion to go to left of the tree
+                int rightHeight = height(root.right);    //recursion to go to right of the tree
+                if (leftHeight > rightHeight)
+                {
+                    return (leftHeight + 1);
+                }
+                else
+                {
+                    return (rightHeight + 1);
+                }
+            }
+        }
+        public void PrintGivenLevel(Node root,int level)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            if (level == 0)
+            {
+                Console.WriteLine(root.data);    //if level 0 means there is only one node
+            }
+            else
+            {
+                PrintGivenLevel(root.left, level - 1);  
+                PrintGivenLevel(root.right, level - 1);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Tree tree = new Tree();
+                tree.root = new Node(1);               //pre defined elements in the tree
+                tree.root.left = new Node(2);
+                tree.root.right = new Node(3);
+                tree.root.left.left = new Node(4);
+                tree.root.left.right = new Node(5);
+                tree.root.right.left = new Node(6);
+                Console.WriteLine("BFS traversal of binary tree is: ");
+                tree.PrintBFS(tree.root);                 //calling print BFS to print tree breath first way
+                Console.ReadKey();
+            }
+            catch (Exception e)                   //catch block to handle any exception if occurs
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+    class Node                               //node class to use it as a structure for a node
+    {
+        public int data;
+        public Node left, right;
+        public Node(int item)
+        {
+            data = item;
+            left = right = null;
+        }
+
+
+    }
+}
---
+++ C# Assignments/DFS.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SB.DFS
+{
+    class Tree
+    {
+        public Node root=null;                  //initializing root node
+        public void PrintInOrder(Node root)     //Inorder function to print tree using inorder traversal
+        {
+            if (root == null)
+            {
+                return;
+            }
+            else
+            {
+                PrintInOrder(root.left);
+                Console.WriteLine(root.data);
+                PrintInOrder(root.right);
+            }
+
+        }
+        public void PrintPreOrder(Node root)     //Preorder function to print tree using inorder traversal
+        {
+            if (root == null)
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine(root.data);
+                PrintInOrder(root.left);
+                PrintInOrder(root.right);
+            }
+
+        }
+        public void PrintPostOrder(Node root)     //Postorder function to print tree using inorder traversal
+        {
+            if (root == null)
+            {
+                return;
+            }
+            else
+            {
+                PrintInOrder(root.left);
+                PrintInOrder(root.right);
+                Console.WriteLine(root.data);
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Tree tree = new Tree();           
+                tree.root = new Node(1);                 //pre defined tree formation
+                tree.root.left = new Node(2);
+                tree.root.right = new Node(3);
+                tree.root.left.left = new Node(4);
+                tree.root.left.right = new Node(5);
+                tree.root.right.left = new Node(6);
+                Console.WriteLine("DFS InOrder traversal of binary tree is: ");
+                tree.PrintInOrder(tree.root);
+                Console.WriteLine("DFS PreOrder traversal of binary tree is: ");
+                tree.PrintPreOrder(tree.root);
+                Console.WriteLine("DFS PostOrder traversal of binary tree is: ");
+                tree.PrintPostOrder(tree.root);
+
+                Console.ReadKey();
+            }
+            catch(Exception e)        //catch block to catch any exception if it occurs
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+    class Node                       //node class that act as a structure for any node
+    {
+        public int data;
+        public Node left, right;
+        public Node(int item)
+        {
+            data = item;
+            left = right = null;
+        }
+
+
+    }
+}
