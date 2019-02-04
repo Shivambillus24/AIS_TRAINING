e95afd1688dca3715e113565419b7d14e6d8f73d
Shivambillus24 <47132484+Shivambillus24@users.noreply.github.com>
2019-02-04 21:00:01 +0530

    Add files via upload

---
+++ C# Assignments/InsertionSort.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SB.InsertionSort
+{
+    class Program
+    {
+        public static void InsertionSort(int[] array , int length)   //function to print the sorted list
+        {
+            int i;
+            for(i=1;i<length;i++)
+            {
+                int j = i;
+                while (j > 0)                             //loop to check till the first element of the array
+                {
+                    if (array[j - 1] > array[j])          //if the previous number is smaller swap them
+                    {
+                        int temp = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = temp;
+                        j--;
+                    }
+                    else
+                    {
+                        j--;
+                    }
+                }
+
+            }
+            Console.WriteLine("The Sorted array is:");
+            for (i = 0; i < length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                int i, length;
+                Console.WriteLine("Enter the length of the array: ");  
+                length = Convert.ToInt32(Console.ReadLine());           //input the length of the array    
+                if (length <= 0)
+                {
+                    Console.WriteLine("There can not be any sorting if the input length is 0:"); 
+                }
+                else
+                {
+                    Console.WriteLine("Enter the elements of the array:");
+                    int[] array = new int[length];
+                    for (i = 0; i < length; i++)
+                    {
+                        array[i] = Convert.ToInt32(Console.ReadLine());  //input the elements in the array
+
+                    }
+                    Program.InsertionSort(array, length);
+                    Console.ReadKey();
+                }    
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);       //print any exception if it occurs
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C# Assignments/Stack.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SB.Stack
+{
+
+    class Program
+    {
+        private int top,maxSize;
+        public void Push(int[] myStack,int j)      //push function to push elements in the stack
+        {
+            if (top == maxSize - 1)                //condition to check if the stack is full or not
+            {
+                Console.WriteLine("The stack is full!");
+            }
+            else
+            {
+                top = top + 1;
+                myStack[top] = j;                   //pushing the element in the stack
+            }
+        }
+        public void Pop(int[] myStack)              //function to pop element from stack
+        {
+            if (top == -1)                           //condition to check if stack is empty or not
+            {
+                Console.WriteLine("The stack is empty:");
+            }
+            else
+            {
+                top--;
+            }
+        }
+        public void PrintElements(int[] myStack)
+        {
+            int i;
+            if (top == -1)
+            {
+                Console.WriteLine("The stack is empty:");
+            }
+            else
+            {
+                Console.WriteLine("The stack elements are:");
+                for (i = 0; i <= top; i++)
+                {
+                    Console.WriteLine(myStack[i]);               //printing the elements in stack
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Program p = new Program();
+                int action;
+                p.top = -1;
+                char answer = 'y';
+                Console.WriteLine("Enter the max size of stack:");
+                p.maxSize = Convert.ToInt32(Console.ReadLine());
+                if (p.maxSize <= 0)              //condition to check if the user input is valid or not
+                {
+                    Console.WriteLine("Stack of desired size can not be possible!");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    int[] myStack = new int[p.maxSize];
+                    while (answer == 'y')
+                    {
+                        Console.WriteLine("To push into stack press 1:");
+                        Console.WriteLine("To pop from stack press 2:");
+                        Console.WriteLine("To print all the elements in stack press 3:");
+                        action = Convert.ToInt32(Console.ReadLine());
+                        switch (action)
+                        {
+                            case 1:
+                                {
+                                    int j;
+                                    Console.WriteLine("Enter the element you want to push:");
+                                    j = Convert.ToInt32(Console.ReadLine());
+                                    p.Push(myStack, j);
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    p.Pop(myStack);
+                                    break;
+
+                                }
+                            case 3:
+                                {
+                                    p.PrintElements(myStack);
+                                    break;
+                                }
+                            default:
+                                {
+                                    Console.WriteLine("please enter a valid choice:");
+                                    break;
+                                }
+                        }
+                        Console.WriteLine("If you want to repeat press y if not press n:");
+                        answer = Convert.ToChar(Console.ReadLine());
+                    }
+                }
+            }
+            catch(Exception e)                    //catch block if there is any exception or not
+            {
+                Console.WriteLine(e.Message);       
+                Console.Read();
+            }
+
+
+
+        }
+    }
+}
